b1565cd85be5f4371420bd49f4748440e2e91d68
Anushk Mittal <anushkmittal@icloud.com>
2020-05-09 07:31:39 -0400

    added ios app structure

---
+++ ios_app/COVID-19-Vis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  COVID-19-Vis
+//
+//  Created by Anushk Mittal on 5/9/20.
+//  Copyright © 2020 104 Days, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
